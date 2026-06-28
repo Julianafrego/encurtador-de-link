667d888a4eedc404ab52ed9824172da6137d4151
julianafrego <julianafgomes@aluno.uespi.br>
2026-06-28 16:56:37 -0300

    arualizar inicializacao

--- app.py
+++ app.py
@@ -107,15 +107,6 @@ def redirecionar(short_code):
         return jsonify({"erro": "Este código de link não existe!"}), 404
 
 
-if __name__ == '__main__':
-    inicializar_banco()
-
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-
-    print(f"Servidor rodando na porta {port}")
-
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
+    app.run(host="0.0.0.0", port=port, debug=False)
